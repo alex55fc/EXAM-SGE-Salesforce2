459dde9bd2f2b67d740a051fca5ae8d5d5122d33
Gorka Sanz <gorka.sanz@salesianos.edu>
2024-01-22 10:34:17 +0100

    added event fire

--- force-app/main/default/aura/Marcador/MarcadorController.js
+++ force-app/main/default/aura/Marcador/MarcadorController.js
@@ -1,5 +1,15 @@
 ({
     myAction : function(component, event, helper) {
 
+    },
+    manejarEvento : function(component, event, helper) {
+        var pointsToAdd = event.getParam("molePoints");
+        var marcador = component.get("v.marcador");
+        if (pointsToAdd == 0){
+            marcador = 0;
+        }else{
+            marcador += pointsToAdd;
+        }
+        component.set("v.marcador", marcador);
     }
 })
--- force-app/main/default/aura/Topo/TopoController.js
+++ force-app/main/default/aura/Topo/TopoController.js
@@ -2,15 +2,26 @@
 ({
     doInit: function(cmp) {
         var topo = cmp.find("topo");
-        Math.random() < 0.5 ? $A.util.addClass(topo, 'active') : $A.util.addClass(topo, 'inactive') ;
+        //Math.random() < 0.5 ? $A.util.addClass(topo, 'active') : $A.util.addClass(topo, 'inactive') ;
         
-        // var number =Math.random() ;
-        // if(number < 0.5){
-        //     $A.util.addClass(cmp, 'active');
-        // }else{
-        //     $A.util.addClass(cmp, 'inactive');
-        // }
+        var number =Math.random() ;
+        if(number < 0.5){
+            $A.util.addClass(cmp, 'active');
+            cmp.set("v.isTopo",true)
+        }else{
+            $A.util.addClass(cmp, 'inactive');
+            cmp.set("v.isTopo",false)
+        }
     
-    }
+    },
+    topoClick: function(cmp, event, helper) {
+        // this function trigger an event
+        // fire the event using that event name
+        var isTopo = cmp.get("v.isTopo");
+        var evt = $A.get("e.c:Puntuacion");
+        evt.setParams({
+            "molePoints": isTopo ? 2 : 0 
+        });
+        evt.fire();
 })
 
