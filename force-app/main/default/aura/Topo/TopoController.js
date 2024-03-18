ff2f837bbc905a177aecca51b645ff3ca33d0bc6
alexa <alexander.55.fc@gmail.com>
2024-03-18 09:26:25 +0100

    punto funcional del init, modificar para que al hacer click en toos ahi se sume un molepoint

--- force-app/main/default/aura/Topo/TopoController.js
+++ force-app/main/default/aura/Topo/TopoController.js
@@ -1,5 +1,6 @@
 
 ({
+
     doInit: function(cmp) {
         // Lista de identificadores de los topos
         var topoIds = ["topo", "topo1", "topo2", "topo3", "topo4", "topo5", "topo6"];
@@ -30,20 +31,16 @@
 
         // Comprobar si el topo tiene la clase 'active'
         var isActive = topo.classList.contains('active');
-        if (isActive) {
-            toposActivos--;
-            if(toposActivos == 0){
-                var molePoints = isActive ? 1 : 0;
     
-                // Crear y lanzar el evento con los puntos correspondientes
-                var evt = $A.get("e.c:Puntuacion");
-                evt.setParams({
-                    "molePoint": molePoints
-                });
-                evt.fire();
-            }
-        }
-       
+        // Definir la cantidad de puntos basados en si el topo está activo o no
+        var molePoints = isActive ? 2 : 0;
+    
+        // Crear y lanzar el evento con los puntos correspondientes
+        var evt = $A.get("e.c:Puntuacion");
+        evt.setParams({
+            "molePoint": molePoints
+        });
+        evt.fire();
     }
 })
 
